cc8cf5d2a1f500bcbc077b70ab155cf5b76941e3
Hayashi Yuki <yuki.hayashi@gzeal.co.jp>
2025-06-30 17:57:29 +0900

    ミニマップをカメラを用いるものに変更

--- Assets/Scripts/Api/ApiClient.cs
+++ Assets/Scripts/Api/ApiClient.cs
@@ -3,6 +3,11 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 
+public class ApiData<T> {
+  public int code;
+  public string err_msg;
+  public T result;
+}
 
 public class ApiClient :MonoBehaviour {
   static ApiClient instance = null;
--- Assets/Scripts/Api/ApiData.cs
+++ Assets/Scripts/Api/ApiData.cs
@@ -1,7 +1,2 @@
 using UnityEngine;
 
-public class ApiData<T> {
-  public int code;
-  public string err_msg;
-  public T result;
-}
