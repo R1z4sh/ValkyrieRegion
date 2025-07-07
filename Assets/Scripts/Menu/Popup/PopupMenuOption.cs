49581af87eea9a54440413c94f929a5403731eca
R1z_ECC <hayashi.yuuki0702@gmail.com>
2025-07-07 21:43:55 +0900

    no message

--- Assets/Scripts/Menu/Popup/PopupMenuOption.cs
+++ Assets/Scripts/Menu/Popup/PopupMenuOption.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class PopupMenuOption : PopupBase<int , bool> {
   [SerializeField] private Button closeButton = null;
   [SerializeField] private Button debugMenuButton = null;
+
   public override void Initialize(int none , System.Threading.Tasks.TaskCompletionSource<bool> tcs) {
     base.Initialize(none , tcs);
     closeButton.onClick.AddListener(() => CloseWithResult(true));
